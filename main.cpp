ae8ba157a9119609d3fbb5571a9d76daaf546811
Avishka Chanuka <86764648+AvishkaChanuka@users.noreply.github.com>
2023-01-06 17:03:45 +0530

    Chapter 1
    Complete the first chapter of the book

--- main.cpp
+++ main.cpp
@@ -1,6 +1,55 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace sf;
+
+int main(){
+
+    VideoMode vm(1920,1080);
+
+    RenderWindow window(vm,"Timber",Style::Fullscreen);
+
+    Texture textureBackground;
+    textureBackground.loadFromFile("../graphics/background.png"); //add path according to the position of .exe file
+
+    Sprite spriteBackground;
+    spriteBackground.setTexture(textureBackground);
+    spriteBackground.setPosition(0,0);
+
+    while (window.isOpen())
+    {
+
+        /*
+         *******************************************
+         * Handle the player's input
+         *******************************************
+        */
+
+        if(Keyboard::isKeyPressed(Keyboard::Escape))
+        {
+            window.close();
+        }
+
+        /*
+         *******************************************
+         * Update the scene
+         *******************************************
+        */
+
+        /*
+         *******************************************
+         * Draw the scene
+         *******************************************
+        */
+
+        //Clear everything from the last frame
+        window.clear();
+
+        //Draw our game scene here
+        window.draw(spriteBackground);
+
+        //Show everything we just drew
+        window.display();
+    }
+
     return 0;
 }
